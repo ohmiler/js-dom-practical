0677d04ef1fb26e49ce3ea22ebad632c45ef0fdf
Patiphan Phengpao <misterpatipan123@gmail.com>
2022-09-13 20:20:20 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -6,21 +6,23 @@ const addToCart = document.querySelector(".add-to-cart");
 const cartAdded = document.querySelector(".cart-added");
 
 redColor.addEventListener("click", () => {
-    productTitle.innerHTML = "Red Mercedes-Benz 2022";
+    // do something
+    productImg.style.backgroundImage = "url('./img/benz-red.jpeg')";
+    productTitle.innerHTML = "Red Mercedes-Benz";
     productTitle.style.color = "red";
     addToCart.style.backgroundColor = "red";
     addToCart.style.color = "white";
     addToCart.style.border = "1px solid red";
-    productImg.style.backgroundImage = "url('./img/benz-red.jpeg')";
 })
 
 whiteColor.addEventListener("click", () => {
-    productTitle.innerHTML = "White Mercedes-Benz 2022";
+    // do something
+    productImg.style.backgroundImage = "url('./img/benz-white.jpeg')";
+    productTitle.innerHTML = "White Mercedes-Benz";
     productTitle.style.color = "black";
     addToCart.style.backgroundColor = "white";
     addToCart.style.color = "black";
     addToCart.style.border = "1px solid black";
-    productImg.style.backgroundImage = "url('./img/benz-white.jpeg')";
 })
 
 addToCart.addEventListener("click", () => {
